e00fad0f7fcd6d59c21c25d03f4f31f4f083ddb3
sphakamiso-git <sparngobese@gmail.com>
2022-02-07 13:41:53 +0200

    Add project files.

---
+++ OnlineShop/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Data;
+using OnlineShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        private ApplicationDbContext _db; 
+
+        public ProductTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            //var data = _db.ProductTypes.ToList();
+            return View(_db.ProductTypes.ToList());
+        }
+
+        //GET:Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //POST:Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductTypes producttypes)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.ProductTypes.Add(producttypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(actionName: nameof(Index));
+            }
+            return View(producttypes);
+        }
+
+        //GET:Edit
+        public ActionResult Edit(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var productType = _db.ProductTypes.Find(id);
+            if(productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+        //POST:Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(ProductTypes producttypes)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(producttypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(actionName: nameof(Index));
+            }
+            return View(producttypes);
+        }
+
+        //GET:Delete
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var productType = _db.ProductTypes.Find(id);
+            if (productType == null)
+            {
+                return NotFound();
+            }
+            return View(productType);
+        }
+
+        //POST:Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public  ActionResult Details(ProductTypes producttypes)
+        { 
+            return View(producttypes);
+        }
+
+    }
+}
